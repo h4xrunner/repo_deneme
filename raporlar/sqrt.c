3e0e25fc68c1272adb280d76bec0d73931b18875
h4xrunner <109393220+h4xrunner@users.noreply.github.com>
2024-02-11 17:19:52 +0300

    Update sqrt.c

--- raporlar/sqrt.c
+++ raporlar/sqrt.c
@@ -1,3 +1,21 @@
 int mySqrt(int x) {
-return sqrt(x);
+long num=0;
+int square=0;
+if(x==0){
+    return 0;
+}
+while(x>=num){
+    
+    if(square ==x|| num*num>x){
+        return num-1;
+    }else{
+        square=num*num;
+        num++;
+    }
+  }
+if(num==0){
+    return 0;
+}else{
+    return num-1;
+}
 }
